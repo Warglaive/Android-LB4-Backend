c4f036996ba47bb2623e6e6580da49fdf26e8f2d
phuynh0211 <60883519+phuynh0211@users.noreply.github.com>
2022-05-30 00:01:59 +0200

    adding file

--- src/application.ts
+++ src/application.ts
@@ -16,10 +16,12 @@ import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
+import multer from 'multer';
 import {RepositoryMixin} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
+import {FILE_UPLOAD_SERVICE, STORAGE_DIRECTORY} from './keys';
 import {MySequence} from './sequence';
 
 export {ApplicationConfig};
@@ -42,6 +44,9 @@ export class Prj4GroupAndroidGroup3Application extends BootMixin(
     });
     this.component(RestExplorerComponent);
 
+    // Configure file upload with multer options
+    this.configureFileUpload(options.fileStorageDirectory);
+
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
     this.bootOptions = {
@@ -60,5 +65,22 @@ export class Prj4GroupAndroidGroup3Application extends BootMixin(
     // Bind datasource
     this.dataSource(MongoDataSource, UserServiceBindings.DATASOURCE_NAME);
     // ------------- END OF SNIPPET -------------
+  }
+
+  protected configureFileUpload(destination?: string) {
+    // Upload files to `dist/.sandbox` by default
+    destination = destination ?? path.join(__dirname, '../.sandbox');
+    this.bind(STORAGE_DIRECTORY).to(destination);
+    const multerOptions: multer.Options = {
+      storage: multer.diskStorage({
+        destination,
+        // Use the original file name as is
+        filename: (req, file, cb) => {
+          cb(null, file.originalname);
+        },
+      }),
+    };
+    // Configure the file upload service with multer options
+    this.configure(FILE_UPLOAD_SERVICE).to(multerOptions);
   }
 }
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from './ping.controller';
 export * from './user.controller';
 export * from './product.controller';
+export * from './file-download.controller';
+export * from './file-upload.controller';
